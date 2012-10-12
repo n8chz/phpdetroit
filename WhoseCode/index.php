a7d0b95b67f5c87f9fc0a41553b4c54889a82b05
lincolnwebs <lincolnwebs@gmail.com>
2012-10-12 10:06:55 -0400

    WhoseCode starter

---
+++ WhoseCode/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Please document your classes and functions like good lads.
+ * 
+ * @package WhoseCode
+ * @copyright PHPDetroit 2012
+ * @license MIT (license.txt)
+ */
+ 
+// Cats and dogs, living together.... mass hysteria!
+

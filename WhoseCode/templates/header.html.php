26963db57ec758fd91e6a1b52d7dbf61aab3deab
Lorraine Lee <n8chz@yahoo.ca>
2012-10-13 20:14:49 -0300

    Update WhoseCode/templates/header.html.php
    background color hue to reflect day of the week

--- WhoseCode/templates/header.html.php
+++ WhoseCode/templates/header.html.php
@@ -3,5 +3,11 @@
 <head>
     <meta charset="utf-8">
     <title>PHP Detroit "Whose Code" App</title>  
+    <style>
+    body {
+     /* background color hue to reflect day of the week */
+     background:hsl(<?php echo floor(date("w")*360.0/7.0+0.5) ?>,100%,80%);
+    }
+    </style>
 </head>
 <body>
